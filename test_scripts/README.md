d438fc618d06081bbf77ac4b9332f22d6c0d9a99
Ramiro González Maciel <ramiro@snoopconsulting.com>
2019-05-22 08:07:30 -0300

    Add test scripts

---
+++ test_scripts/README.md
@@ -0,0 +1,16 @@
+# Shel script tests
+
+This folder contains shell script tests.
+
+
+FIXME convert them to golang unit tests
+
+## Running
+
+Run them from the root cfssl folder as:
+
+```bash
+make
+test_scripts/*.sh
+```
+
---
+++ test_scripts/test_csr_emailaddress_in_subject.sh
@@ -0,0 +1,44 @@
+cat <<EOF > /tmp/config.json
+{"signing":{"default":{"expiry":"43800h","usages":["signing","key encipherment","client auth"]}}}
+EOF
+
+cat <<EOF > /tmp/csr_dup.json
+{
+  "CN":"hub.test.mowali.com",
+  "hosts":[
+    "hub1.test.mowali.com",
+    "hub2.test.mowali.com",
+    "163.10.5.24",
+    "163.10.5.22",
+    "beatrice.wagate@mowali.com"
+    ],
+  "key":{
+    "size":4096,
+    "algo":"rsa"
+  },
+  "names":[
+    {
+      "CN":"hub.test.mowali.com",
+      "O":"Mowali",
+      "OU":"PKI"
+    }
+  ]
+}
+EOF
+
+
+cat /tmp/csr_dup.json \
+| bin/cfssl genkey -loglevel $LOG_LEVEL -config=/tmp/config.json - \
+| jq -r '.csr' \
+| openssl req  -text -noout \
+| grep "DNS:hub1.test.mowali.com, DNS:hub2.test.mowali.com, email:beatrice.wagate@mowali.com, IP Address:163.10.5.24, IP Address:163.10.5.22"
+
+if [ $? -ne 0 ]; then
+    echo "test $0 failed. See log output"
+    exit -1
+else
+    echo "test $0 passed"
+fi
+
+
+
